1793d6526e685724af904771e45d0b6eb70b6c91
Kouji Watanabe <kjwtnb@gmail.com>
2015-02-16 08:33:44 +0900

    add sample

--- src/main/scala/how2twirl/MainApp.scala
+++ src/main/scala/how2twirl/MainApp.scala
@@ -2,10 +2,30 @@ package how2twirl
 
 object MainApp {
   def main(args:Array[String]){
-    sample001()
-    sample002()
-    sample003()
-    sample004()
+//    sample001()
+//    sample002()
+//    sample003()
+//    sample004()
+//    sample005()
+//    sample006()
+//    sample007()
+    sample008()
+  }
+  def sample008(){
+    val content = html.sample008("uhoho")
+    println(content)
+  }
+  def sample007(){
+    val content = html.sample007()
+    println(content)
+  }
+  def sample006(){
+    val content = html.sample006()
+    println(content)
+  }
+  def sample005(){
+    val content = html.sample005()
+    println(content)
   }
   def sample004(){
     val customer = new Customer("customer_taro")
